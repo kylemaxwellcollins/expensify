5bad534fec96edef7ed67e5640a3e6393d2be45b
kylemaxwellcollins <kylemaxwellcollins@gmail.com>
2019-01-04 21:31:53 -0500

    Added date picker and moment for time createdAt. Added add and edit expense features :wrench:

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
+import ExpenseForm from "./ExpenseForm";
+import { editExpense } from "../actions/expenses";
 
-const EditExpensePage = () => (
-  <div>
-    This is from my edit expense component
-  </div>
-)
+const EditExpensePage = props => {
+  // console.log(props.expense)
+  return (
+    <div>
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={expense => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          props.history.push("/");
+        }}
+      />
+    </div>
+  );
+};
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find(
+      expense => expense.id === props.match.params.id
+    )
+  };
+};
+
+export default connect(mapStateToProps)(EditExpensePage);
